710cc4f1e979ca93d009912c157a183b9a47b5a9
Hades <hadesmercy@163.com>
2022-11-08 12:42:59 +0800

    make

--- py/作业/mqueue.py
+++ py/作业/mqueue.py
@@ -1,34 +1,76 @@
-import queue
-int(input())
+class queuee:
+    lens = 0
+    stack = []
+    
+    def __init__(self, lenth):
+        self.len = int(lenth)
 
-my_queue = queue.Queue(100)
+    def put(self, number):
+        
+        self.stack.append(number)
+        self.lens = self.lens + 1
+    def get(self):
+        if  self.lens == 0:
+            return 
+        temp = self.stack[0]
+        del self.stack[0]
+        self.lens -= 1
+        return temp
+    def qsize(self):
+        return self.lens
+
+
+si = int(input())
+
+my_queue = queuee(si)
 number = list(map(int,input().split()))
 for i in range(len(number)):
     my_queue.put(number[i])
+#初始化队列
+the_out_num = []
 
 
-order = list(input().split())
-if order[0] == 'out':
-    del order[0]
-    for i in range(int(order[0])):
-        my_queue.get()
-if order[0] == 'in':
-    del order[0]
-    order = list(map(int,order))
-    for i in range(len(order)):
-        my_queue.put(order[i])
+order1 = input().split()
+if order1[0] == 'out':
+    del order1[0]
+    tt = int(order1[0])
+    if tt > my_queue.lens:
+        tt = my_queue.lens
+    for i in range(tt):
+        the_out_num.append(my_queue.get())
 
-order = list(input().split())
-if order[0] == 'out':
-    del order[0]
-    for i in range(int(order[0])):
-        my_queue.get()
-if order[0] == 'in':
-    del order[0]
-    order = list(map(int,order))
-    for i in range(len(order)):
-        my_queue.put(order[i])
 
+if order1[0] == 'in':
+    del order1[0]
+    for i in range(len(order1)):
+        my_queue.put(int(order1[i]))
+
+order1 = input().split()
+if order1[0] == 'out':
+    del order1[0]
+    tt = int(order1[0])
+    if tt > my_queue.lens:
+        tt = my_queue.lens
+    for i in range(tt):
+        the_out_num.append(my_queue.get())
+
+
+if order1[0] == 'in':
+    del order1[0]
+    for i in range(len(order1)):
+        my_queue.put(int(order1[i]))
+
+b = []
 for i in range(my_queue.qsize()):
-    print(my_queue.get())
+    b.append(my_queue.get())
+    
 
+if len(b) == 0:
+    print("len = 0")
+else:
+    print("len = " + str(len(b)) + ", data = " + " ".join(str(st) for (st) in b))
+
+if len(the_out_num) == 0:
+    print("len = 0")
+else:
+    print("len = " + str(len(the_out_num)) + ", data = " + " ".join(str(st) for (st) in the_out_num))
